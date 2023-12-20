dda6d2ecb357f0fab04483b4cf8d383ca3d6fcc8
State Of The Art <shuqinnq08@gmail.com>
2023-12-19 23:14:49 -0500

    Update code style

--- Frontend/stars.js
+++ Frontend/stars.js
@@ -1,16 +1,16 @@
 function getRandom(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}			
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 var canvas = document.getElementById('starfield'),
-context = canvas.getContext('2d'),
-stars = 600,
-colorrange = [0,60,240];
+	context = canvas.getContext('2d'),
+	stars = 600,
+	colorrange = [0, 60, 240];
 for (var i = 0; i < stars; i++) {
 	var x = Math.random() * canvas.offsetWidth;
 	y = Math.random() * canvas.offsetHeight,
-	radius = Math.random() * 1.2,
-	hue = colorrange[getRandom(0,colorrange.length - 1)],
-	sat = getRandom(50,100);
+		radius = Math.random() * 1.2,
+		hue = colorrange[getRandom(0, colorrange.length - 1)],
+		sat = getRandom(50, 100);
 	context.beginPath();
 	context.arc(x, y, radius, 0, 360);
 	context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
